91bc0d2095c9457a4303c9e9b2c1fbf5f554c546
Rodolphe Pineau <pineau@rti-zone.org>
2005-04-10 21:08:16 +0000

    Started Meade ASU code integration

--- AutoStarX.save/Serial.h
+++ AutoStarX.save/Serial.h
@@ -7,21 +7,6 @@
  *
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <errno.h>
-#include <paths.h>
-#include <termios.h>
-#include <sysexits.h>
-#include <sys/param.h>
-#include <sys/select.h>
-#include <sys/time.h>
-#include <time.h>
-#include <stdbool.h>
 
 #include <CoreFoundation/CoreFoundation.h>
 
@@ -44,20 +29,11 @@ public:
     
     virtual OSErr FindPorts(io_iterator_t *matchingServices);
     virtual CFMutableArrayRef GetPortList(io_iterator_t serialPortIterator);
-    virtual bool OpenSerialPort(const char *bsdPath, int speed);
-    virtual void CloseSerialPort();
     virtual SInt32 getCount();
     virtual CFStringRef getPortName(int index);
     virtual CFStringRef getPortPath(int index);
-    virtual bool SetSpeed(int speed);
-    virtual bool SendData(Byte * dataOut, int length);
-    virtual bool ReadData(Byte * dataIn, int length);
     
     //public data
     CFMutableArrayRef   mPortArray;
     
-private:
-    // Hold the original termios attributes so we can reset them
-    struct termios mOriginalTTYAttrs;
-    int mSerialPortHandle;
 };
