08dfdb69701a5527163bd95ded06ba54e7a88ab2
Rodolphe Pineau <pineau@rti-zone.org>
2006-07-03 02:26:21 +0000

    re-import v 1.4 as head

---
+++ AutoStarX/FileSelector.h
@@ -0,0 +1,21 @@
+/*
+ *  fileselect.h
+ *  AutoStar
+ *
+ *  Created by roro on 11/16/04.
+ *  Copyright 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <Carbon/Carbon.h>
+class FileSelector
+{
+public:
+    FileSelector();
+    virtual ~FileSelector();
+    virtual FSSpec FileSelect();
+    
+private :
+
+};
+
